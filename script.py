075c85a616456d647487dd1f2c9b411b52c50062
Steven Sim <stevensim226@gmail.com>
2020-09-02 11:10:29 +0700

    updated ci/cd

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
 print("Hello World!")
 print("This is from Travis CI!")
 print(11 + 11)
+
+try:
+    import numpy
+except:
+    print("Cannot import numpy")
+else:
+    print("Successfully imported numpy")
